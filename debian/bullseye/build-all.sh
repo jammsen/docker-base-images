4510fb07a139ea423abbb0bcbcc96ed23d971e42
jammsen <2270806+jammsen@users.noreply.github.com>
2022-01-08 19:55:05 +0100

    Added Debian Bullseye and changed a lib in the Ubuntu verion

---
+++ debian/bullseye/build-all.sh
@@ -0,0 +1,15 @@
+echo "#############################################################################"
+echo "# Building base:debian-bullseye image ..."
+echo "#############################################################################"
+docker build --tag jammsen/base:debian-bullseye .
+echo "#############################################################################"
+echo "# Building base:wine-stable-debian-bullseye image ..."
+echo "#############################################################################"
+docker build --tag jammsen/base:wine-stable-debian-bullseye wine
+#echo "#############################################################################"
+#echo "# Building base:wine-stable-xvfb-winetricks-debian-bullseye image ..."
+#echo "#############################################################################"
+#docker build --tag jammsen/base:wine-stable-xvfb-winetricks-debian-bullseye wine-xvfb-winetricks
+echo "#############################################################################"
+echo "# All done building images ..."
+echo "#############################################################################"
