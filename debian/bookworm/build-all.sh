152f8b5678711ec37bb5c0ae78f7be2f6115bb0f
Sebastian Schmidt <2270806+jammsen@users.noreply.github.com>
2024-10-03 13:21:01 +0200

    extended bookworm buildscripts and changed way to get the key and repo, according to the documentation

--- debian/bookworm/build-all.sh
+++ debian/bookworm/build-all.sh
@@ -1,15 +1,16 @@
+#!/usr/bin/env bash
 echo "#############################################################################"
 echo "# Building base:debian-bookworm image ..."
 echo "#############################################################################"
-docker build --tag jammsen/base:debian-bookworm .
+docker build "$@" --tag jammsen/base:debian-bookworm .
 echo "#############################################################################"
 echo "# Building base:wine-stable-debian-bookworm image ..."
 echo "#############################################################################"
-docker build --tag jammsen/base:wine-stable-debian-bookworm wine
+docker build "$@" --tag jammsen/base:wine-stable-debian-bookworm wine
 #echo "#############################################################################"
 #echo "# Building base:wine-stable-xvfb-winetricks-debian-bookworm image ..."
 #echo "#############################################################################"
-#docker build --tag jammsen/base:wine-stable-xvfb-winetricks-debian-bookworm wine-xvfb-winetricks
+#docker build "$@" --tag jammsen/base:wine-stable-xvfb-winetricks-debian-bookworm wine-xvfb-winetricks
 echo "#############################################################################"
 echo "# All done building images ..."
 echo "#############################################################################"
--- debian/bullseye/build-all.sh
+++ debian/bullseye/build-all.sh
@@ -1,15 +1,16 @@
+#!/usr/bin/env bash
 echo "#############################################################################"
 echo "# Building base:debian-bullseye image ..."
 echo "#############################################################################"
-docker build --tag jammsen/base:debian-bullseye .
+docker build "$@" --tag jammsen/base:debian-bullseye .
 echo "#############################################################################"
 echo "# Building base:wine-stable-debian-bullseye image ..."
 echo "#############################################################################"
-docker build --tag jammsen/base:wine-stable-debian-bullseye wine
+docker build "$@" --tag jammsen/base:wine-stable-debian-bullseye wine
 #echo "#############################################################################"
 #echo "# Building base:wine-stable-xvfb-winetricks-debian-bullseye image ..."
 #echo "#############################################################################"
-#docker build --tag jammsen/base:wine-stable-xvfb-winetricks-debian-bullseye wine-xvfb-winetricks
+#docker build "$@" --tag jammsen/base:wine-stable-xvfb-winetricks-debian-bullseye wine-xvfb-winetricks
 echo "#############################################################################"
 echo "# All done building images ..."
 echo "#############################################################################"
